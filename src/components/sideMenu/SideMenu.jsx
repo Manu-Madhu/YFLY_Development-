e2e50754cc32d7bf8222636171b06bc467ef3187
Manu-Madhu <manum66466@gmail.com>
2024-01-22 11:56:37 +0530

    side bar totally completed

--- src/components/sideMenu/SideMenu.jsx
+++ src/components/sideMenu/SideMenu.jsx
@@ -14,7 +14,7 @@ const SideMenu = () => {
   const location = useLocation();
   return (
     <div
-      className={`hidden pt-10 min-h-screen bg-gradient-to-l from-white to-[#f5f6f8] ${
+      className={`hidden relative pt-10 min-h-screen bg-gradient-to-l from-white to-[#f5f6f8] ${
         sideArrow ? "md:w-1/5 " : "w-[120px] flex flex-col items-center"
       } ${user?.role === "student" ? "hidden" : "md:flex"}`}
     >
@@ -62,18 +62,18 @@ const SideMenu = () => {
         )}
 
         {/* Arrow */}
-        <div className="w-full flex items-end justify-end mt-10">
+        <div className="w-full flex absolute top-64 -right-3 items-end justify-end mt-10">
           {sideArrow ? (
             <MdOutlineKeyboardDoubleArrowLeft
               onClick={() => setSideArrow(false)}
-              size={30}
-              className="bg-primary_colors rounded-full p-1 text-white shadow-lg cursor-pointer hover:scale-105 ease-in-out duration-300"
+              size={40}
+              className="bg-primary_colors rounded-full p-2 text-white shadow-lg cursor-pointer hover:scale-105 ease-in-out duration-300"
             />
           ) : (
             <MdOutlineKeyboardDoubleArrowRight
               onClick={() => setSideArrow(true)}
-              size={30}
-              className="bg-primary_colors rounded-full p-1 text-white shadow-lg cursor-pointer hover:scale-105 ease-in-out duration-300"
+              size={40}
+              className="bg-primary_colors rounded-full p-2 text-white shadow-lg cursor-pointer hover:scale-105 ease-in-out duration-300"
             />
           )}
         </div>
