5bb2dc2d62f4883e99f0a010a4e47efc54554e4f
Manu-Madhu <manum66466@gmail.com>
2024-01-23 16:33:38 +0530

    update on search

--- src/components/search/Application.jsx
+++ src/components/search/Application.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import CommonTable from "../Table/Commontable";
 import Pagination from "../Pagination";
 import { useSelector } from "react-redux";
+import { Intake } from "../../data/Dashboard";
 
 const Application = () => {
   //   const [data, setData] = useState();
 
   const [page, setPage] = useState(1);
   const [entries, setEntries] = useState(10);
-  const [modal, setModal] = useState(false);
 
   const data = useSelector((state) => state.search.searchData);
 
@@ -16,29 +16,6 @@ const Application = () => {
     <div className="w-full h-full text-black pt-10 pb-28">
       <div className="flex flex-col md:flex-row justify-between">
         <h1 className="text-primary_colors text-2xl font-bold">Applications</h1>
-
-        {/* filter & Application */}
-        {/* <div className="flex flex-col md:flex-row gap-3 mt-5 md:mt-0">
-        <select
-          name="intake"
-          id=""
-          onChange={filterHandler}
-          className="px-5 text-sm text-gray-700 p-2 rounded-lg border border-primary_colors  focus:outline-none"
-        >
-          <option value="">Intake</option>
-          {Intake.map((items, i) => (
-            <option key={i} value={items?.name}>
-              {items?.name}
-            </option>
-          ))}
-        </select>
-        <button
-          onClick={() => setModal(true)}
-          className="p-2 px-5 text-normal bg-primary_colors text-white rounded-lg hover:scale-105 ease-in-out duration-200"
-        >
-          Add New Application
-        </button>
-      </div> */}
       </div>
 
       {/* Common Table */}
