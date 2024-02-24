b9ca02e293a5001c4c54fd04b638d0d64700160f
knoskimo <abhijithrb91@gmail.com>
2024-02-24 21:20:09 +0530

    commented logs 24/02

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -25,9 +25,9 @@ const useAxiosPrivate = () => {
         const responseIntercept = axiosPrivate.interceptors.response.use(
             response => response,
             async (error) => {
-                console.log("Main error", error)
+                // console.log("Main error", error)
                 const prevRequest = error?.config;
-                console.log("prevRequest", prevRequest)
+                // console.log("prevRequest", prevRequest)
                 if (error?.response?.status === 401 && !prevRequest?.sent) {
                     prevRequest.sent = true;
                     
